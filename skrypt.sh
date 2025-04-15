d48288e919973424dac20680be4712dd77311949
OlekOlekOlekOlek <dsw55813@student.dsw.edu.pl>
2025-04-15 17:11:07 +0200

    Dodano --logs z możliwościa podania liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,12 @@ if ["$1" == "--date" ]; then
 	date
 fi
 
+elif [ "$1" == "--logs" ]; then
+    count=${2:-100}
+    for i in $(seq 1 $count); do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > $filename
+        echo "Skrypt: $0" >> $filename
+        echo "Data: $(date)" >> $filename
+    done
+
